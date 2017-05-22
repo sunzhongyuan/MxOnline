85cd15ff94fc232bb25aa310f92ea3146d6f41ce
sunzhongyuan <sun_zhongyuan@sina.com>
2017-05-22 09:04:43 +0800

    model xadmin 2

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -11,13 +11,12 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-import sys
-module_path = os.path.join(BASE_DIR, 'apps')
-sys.path.append(module_path)
+sys.path.insert(0,os.path.join(BASE_DIR, 'apps'))
+sys.path.insert(0,os.path.join(BASE_DIR, 'extra_apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -40,10 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
+    'courses',
+    'organization',
+    'operation',
+    'xadmin',
+    'crispy_forms'
 ]
 
-AUTH_USER_MODEL = 'users.Userprofile'
+AUTH_USER_MODEL = 'users.UserProfile'
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
@@ -114,15 +118,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/BeiJing'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,6 @@
 # _*_ coding=utf-8 _*_
 from __future__ import unicode_literals
+from datetime import datetime
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
@@ -10,7 +11,7 @@ from django.contrib.auth.models import AbstractUser
 class UserProfile(AbstractUser):
     nick_name = models.CharField(max_length=50,verbose_name=u'昵称',default='')
     birday = models.DateField(verbose_name=u'生日',null=True,blank=True)
-    gender = models.CharField(max_length=10,choices=(('male',u'男'),('female',u'女')),default='female',verbose_name=u'性别')
+    gender = models.CharField(max_length=6,choices=(('male',u'男'),('female',u'女')),default='female',verbose_name=u'性别')
     address = models.CharField(max_length=100,default=u'',verbose_name=u'地址')
     mobile = models.CharField(max_length=11,null=True,blank=True,verbose_name=u'手机号')
     image = models.ImageField(upload_to='image/%Y/%m',default='image/default.png')
@@ -22,4 +23,26 @@ class UserProfile(AbstractUser):
     def __unicode__(self):
         return self.username
 
+
+class EmailVerifyRecord(models.Model):
+    code = models.CharField(max_length=20,verbose_name=u'验证码')
+    email = models.EmailField(max_length=50,verbose_name=u'邮箱')
+    send_type = models.CharField(choices=(('register',u'注册'),('forget',u'找回密码')),max_length=10)
+    send_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = u'邮箱验证码'
+        verbose_name_plural = verbose_name
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100,verbose_name=u'标题')
+    image = models.ImageField(upload_to='banner/%Y/%m',verbose_name=u'轮播图',max_length=100)
+    url = models.URLField(max_length=200,verbose_name=u'访问地址')
+    index = models.IntegerField(default=100,verbose_name=u'顺序')
+    add_time = models.DateTimeField(default=datetime.now,verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'轮播图'
+        verbose_name_plural = verbose_name
 
